837f449c8f2ffc1a500d12addc537ae9d3f668fa
Hugo da Silva Bertoncelo <hugobertoncelo@gmail.com>
2024-12-11 14:39:58 -0300

    ne pasts and configurations

--- src/components/market/info/styles.ts
+++ src/components/market/info/styles.ts
@@ -3,22 +3,15 @@ import { colors, fontFamily } from "@/styles/theme"
 
 export const s = StyleSheet.create({
   container: {
-    width: "100%",
     flexDirection: "row",
-    gap: 16,
+    alignItems: "center",
+    gap: 8,
   },
-  details: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 16,
-    fontFamily: fontFamily.semiBold,
-    color: colors.gray[600],
-  },
-  description: {
+  text: {
+    color: colors.gray[500],
     fontSize: 14,
     fontFamily: fontFamily.regular,
-    color: colors.gray[500],
-    marginTop: 4,
+    lineHeight: 22.4,
+    flex: 1,
   },
 })
